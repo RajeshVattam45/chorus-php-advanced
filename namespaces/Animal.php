d574bf9e756a2a10400765761f2944a73ada94c9
RajeshVattam45 <vattamrajesh45@gmail.com>
2024-10-07 11:24:11 +0530

    Updated comments

--- namespaces/Animal.php
+++ namespaces/Animal.php
@@ -1,21 +1,27 @@
 <?php
 
+// Declare a namespace 'namespaces\Animal'.
 namespace namespaces\Animal;
 
+// Define a class called Animals.
 class Animals {
+    // Public properties to hold the name and color of the animal.
     public $name;
     public $color;
 
-    function __construct($name, $color)
-    {
+    // Constructor method to initialize the object with name and color
+    function __construct($name, $color) {
+        // Assign values to the name and color properties.
         $this->name = $name;
         $this->color = $color;
     }
 
+    // Method to get the name of the animal.
     public function getName() {
         return $this->name;
     }
 
+    // Method to get the color of the animal.
     public function getColor() {
         return $this->color;
     }
--- namespaces/Fruits.php
+++ namespaces/Fruits.php
@@ -1,21 +1,26 @@
 <?php
 
+// Define a namespace 'namespaces\Fruits'.
 namespace namespaces\Fruits;
 
+// Define a class called Fruits.
 class Fruits {
+    // Public properties for name and color.
     public $name;
     public $color;
 
-    function __construct($name, $color)
-    {
+    // Constructor to initialize name and color.
+    function __construct($name, $color) {
         $this->name = $name;
         $this->color = $color;
     }
 
+    // Method to get the name of the fruit.
     public function getName() {
         return $this->name;
     }
     
+    // Method to get the color of the fruit.
     public function getcolor() {
         return $this->color;
     }
